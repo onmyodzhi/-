e67e04ff66a2753ba36696d868bccce37b2313eb
R3ndyyyy <mrkobalt18@gmail.com>
2023-02-17 12:48:37 +0300

    ya reshil

--- isAKon/Program.cs
+++ isAKon/Program.cs
@@ -2,5 +2,21 @@
 {
     System.Console.WriteLine("Сколько слов будет в вашем массиве");
     int stringLength = Convert.ToInt32(Console.ReadLine());
-    string [] word = new string[stringLength - 1];
+    string [] word = new string[stringLength];
+    for (int i = 0; i < word.Length; i++)
+    {
+        System.Console.WriteLine("Enter word");
+        word[i] = Convert.ToString(Console.ReadLine());
+    }
+    return word;
 }
+void PrintString(string[] words)
+{
+    for (int i = 0; i < words.Length; i++)
+    {
+        int wordLen = words[i].Length;
+        if(wordLen <=3 )
+            System.Console.Write(words[i] + " ");
+    }
+}
+PrintString(CreateString());
