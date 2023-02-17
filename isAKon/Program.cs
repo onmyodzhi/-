5489827387021951a3649f29a2501d7287c68f6f
R3ndyyyy <mrkobalt18@gmail.com>
2023-02-17 12:31:52 +0300

    create metod

--- isAKon/Program.cs
+++ isAKon/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿string[] CreateString ()
+{
+    System.Console.WriteLine("Сколько слов будет в вашем массиве");
+    int stringLength = Convert.ToInt32(Console.ReadLine());
+    string [] word = new string[stringLength - 1];
+}
